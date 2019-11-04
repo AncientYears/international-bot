2847ff02d029f7c745b111794ab5b0ea9ac66fe6
Emil <emiluuu40@gmail.com>
2019-11-04 20:03:20 +0100

    Added varibles and logging in.
    A Discord client is now beeing created and logged in at the end.
    
    (In the package.json, various things have been optimized.)

--- app.js
+++ app.js
@@ -6,3 +6,11 @@
     Created: 4.11.2019
     Authors: Emilius123
 */
+
+//-- Variables --\\
+const config = require("./config") //A config doesn't exist yet, but it will. ;)
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+//-- Finally logging in --\\
+client.login(config.Token)
